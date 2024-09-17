b4ff4dd038f5b316a68166873ced8be5d177f21e
Nguyễn Hoàng Thiên <149929660+Thienx2004@users.noreply.github.com>
2024-09-17 07:15:02 +0700

    first commit

---
+++ src/main/java/com/koifarm/koifarmshop/KoiFarmShopApplication.java
@@ -0,0 +1,13 @@
+package com.koifarm.koifarmshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoiFarmShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoiFarmShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koifarm/koifarmshop/KoiFarmShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koifarm.koifarmshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiFarmShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
