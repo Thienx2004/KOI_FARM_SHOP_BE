deb0a0daa4a367ba89ceb3c9ee90c497e98443fa
Nguyễn Hoàng Thiên <149929660+Thienx2004@users.noreply.github.com>
2024-09-19 23:53:38 +0700

    Login v3

--- src/main/java/com/group2/KoiFarmShop/config/SecurityConfig.java
+++ src/main/java/com/group2/KoiFarmShop/config/SecurityConfig.java
@@ -1,16 +1,28 @@
 package com.group2.KoiFarmShop.config;
 
+import com.group2.KoiFarmShop.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
+
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,9 +32,21 @@ public class SecurityConfig{
                                 "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()  // Permit all access to /signIn
                         .anyRequest().authenticated()  // All other requests require authentication
                 )
-                .csrf(AbstractHttpConfigurer::disable); // Disable CSRF protection if not needed
-
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection if not needed
+                .httpBasic(withDefaults());
         return http.build();
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
     }
 }
--- src/main/java/com/group2/KoiFarmShop/service/AccountServiceImp.java
+++ src/main/java/com/group2/KoiFarmShop/service/AccountServiceImp.java
@@ -1,6 +1,7 @@
 package com.group2.KoiFarmShop.service;
 
 
+import com.group2.KoiFarmShop.dto.reponse.ApiReponse;
 import com.group2.KoiFarmShop.dto.request.LoginRequest;
 import com.group2.KoiFarmShop.dto.request.AccountCreationDTO;
 import com.group2.KoiFarmShop.entity.Account;
@@ -10,5 +11,6 @@ public interface AccountServiceImp {
 
     public Account createAccount(AccountCreationDTO accountCreationDTO);
     public Account getAccount(int id);
-    public String login(LoginRequest loginRequest);
+    public ApiReponse login(LoginRequest loginRequest);
+
 }
