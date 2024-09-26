7ec3e64e204cf5bc29c999e0635e8b8bbe1f0fe8
Nguyễn Hoàng Thiên <149929660+Thienx2004@users.noreply.github.com>
2024-09-26 20:00:43 +0700

    add compare

--- src/main/java/com/group2/KoiFarmShop/controller/KoiFishController.java
+++ src/main/java/com/group2/KoiFarmShop/controller/KoiFishController.java
@@ -32,7 +32,7 @@ public class KoiFishController {
         return ApiReponse.<KoiFishPageResponse>builder().data(koiFishList).statusCode(200).build();
     }
 
-    // Lấy 5 cá Koi theo Category và Trang
+    // Lấy cá Koi theo Category và Trang
     @GetMapping("/category")
     public ApiReponse<KoiFishPageResponse> getKoiByCategory(@RequestParam("categoryId") int id, @RequestParam("page") int page,@RequestParam("pageSize") int pageSize) {
         Category category=categoryRepository.findByCategoryID(id);
@@ -44,17 +44,16 @@ public class KoiFishController {
     @GetMapping("/{id}")
     public ApiReponse<KoiFishReponse> getKoiFishById(@PathVariable int id) {
         KoiFishReponse koiFish = koiFishService.getKoiFishById(id);
-        if (koiFish != null) {
+
             return ApiReponse.<KoiFishReponse>builder().data(koiFish).build();
-        } else {
-            throw new AppException(ErrorCode.KOINOTFOUND);
-        }
     }
 
     // Thêm mới cá Koi
     @PostMapping("/add")
     public ApiReponse<KoiFishReponse> addKoiFish(@RequestBody KoiRequest koiFish) {
-
+        if(koiFish.getCategoryId()==0){
+            throw new AppException(ErrorCode.KOINOTFOUND);
+        }
         KoiFishReponse koiFishReponse=koiFishService.addKoiFish(koiFish);
         return ApiReponse.<KoiFishReponse>builder().data(koiFishReponse).message("Thêm Koi thành công").statusCode(200).build();
     }
@@ -63,7 +62,9 @@ public class KoiFishController {
     // Cập nhật cá Koi
     @PutMapping("/update/{id}")
     public ApiReponse<KoiFishReponse> updateKoiFish(@PathVariable int id, @RequestBody KoiRequest koiFish) {
-
+        if(koiFish.getCategoryId()==0){
+            throw new AppException(ErrorCode.KOINOTFOUND);
+        }
         KoiFishReponse koiFishReponse = koiFishService.updateKoiFish(koiFish,id);
         return ApiReponse.<KoiFishReponse>builder().data(koiFishReponse).message("Cập nhật thành công").statusCode(200).build();
     }
@@ -88,5 +89,9 @@ public class KoiFishController {
         return ApiReponse.<KoiFishPageResponse>builder().data(koiFishReponses).build();
     }
 
-
+    @GetMapping("/compare")
+    public ApiReponse<List<KoiFishReponse>> compareKoiFish(@RequestParam("koiFishId1") int id1, @RequestParam("koiFishId2") int id2) {
+        List<KoiFishReponse> koiFishReponses = koiFishService.compareKoiFish(id1, id2);
+        return ApiReponse.<List<KoiFishReponse>>builder().data(koiFishReponses).build();
+    }
 }
