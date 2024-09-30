ca24c30e551144ed2275239bf553bfd45147545c
HaoTranDev2013 <trananhhao225@gmail.com>
2024-09-30 17:24:49 +0700

    firebase2

--- src/main/java/com/group2/KoiFarmShop/controller/FirebaseController.java
+++ src/main/java/com/group2/KoiFarmShop/controller/FirebaseController.java
@@ -2,9 +2,12 @@ package com.group2.KoiFarmShop.controller;
 
 import com.group2.KoiFarmShop.service.FirebaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FirebaseController {
@@ -16,5 +19,12 @@ public class FirebaseController {
     @GetMapping("/image/{fileName}")
     public String getImageUrl(@PathVariable String fileName) {
         return firebaseService.getImageUrl(fileName);
+    }
+
+    @GetMapping("/image-gallery")
+    public String getImageGallery(Model model) {
+        List<String> imageUrls = firebaseService.getImageUrls("KoiAsagi/");
+        model.addAttribute("imageUrls", imageUrls);
+        return "images";
     }
 }
--- src/main/java/com/group2/KoiFarmShop/service/FirebaseService.java
+++ src/main/java/com/group2/KoiFarmShop/service/FirebaseService.java
@@ -2,8 +2,13 @@ package com.group2.KoiFarmShop.service;
 
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.google.firebase.cloud.StorageClient;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FirebaseService {
@@ -21,7 +26,26 @@ public class FirebaseService {
         }
 
 
-        return blob.signUrl(30, java.util.concurrent.TimeUnit.DAYS).toString(); // URL tồn tại trong 14 ngày
+        return blob.signUrl(30, java.util.concurrent.TimeUnit.DAYS).toString(); // URL tồn tại trong 30 ngày
+    }
+
+    private final String bucketName = "koi-farm-shop-5212e.appspot.com";
+
+    public List<String> getImageUrls(String folderName) {
+        List<String> imageUrls = new ArrayList<>();
+
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        Bucket bucket = storage.get(bucketName);
+
+        for (Blob blob : bucket.list(Storage.BlobListOption.prefix(folderName)).iterateAll()) {
+            if (!blob.isDirectory()) {
+                // Tạo URL truy cập công khai
+                String imageUrl = String.format("https://storage.googleapis.com/%s/%s", bucketName, blob.getName());
+                imageUrls.add(imageUrl);
+            }
+        }
+
+        return imageUrls;
     }
 
 
