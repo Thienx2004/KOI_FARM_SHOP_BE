321f8e4b14ef259600e806cfb0665f7663fa12f9
loc <locttse182329@fpt.edu.vn>
2024-10-01 07:40:08 +0700

    payment table

--- src/main/java/com/group2/KoiFarmShop/entity/Payment.java
+++ src/main/java/com/group2/KoiFarmShop/entity/Payment.java
@@ -1,6 +1,30 @@
 package com.group2.KoiFarmShop.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int paymentID;
+
+    private String transactionCode;
+    private double amount;
+
+    @OneToOne
+    @JoinColumn(name = "orderID")
+    private Orders order;
+
+    private String status;
+    private Date paymentDate;
 
 }
