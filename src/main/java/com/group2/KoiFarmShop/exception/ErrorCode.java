7fc1ce33d7138f0399654f9dd71f093289b29bd3
loc <locttse182329@fpt.edu.vn>
2024-10-06 00:37:27 +0700

    ràng buộc tạo mã

--- src/main/java/com/group2/KoiFarmShop/exception/ErrorCode.java
+++ src/main/java/com/group2/KoiFarmShop/exception/ErrorCode.java
@@ -18,7 +18,8 @@ public enum ErrorCode {
     TRANSACTION_INVALID(5010, "Mã thanh toán này không hợp lệ"),
     SAVE_FAILED(5011, "Lưu thất bại"),
     CONSIGNMENT_NOT_FOUND(5012, "Đơn ký gửi không tìm thấy"),
-    PROMOTION_INVALID(5013, "Mã này không hợp lệ")
+    PROMOTION_INVALID(5013, "Mã này không hợp lệ"),
+    PROMOTION_DATE_OVERLAP(5014, "Đã có mã khuyến mãi trong thời gian này")
 
     ;
 
