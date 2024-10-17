26790c458eeba524d02c99b2a7917f2abc58222c
Nguyễn Hoàng Thiên <149929660+Thienx2004@users.noreply.github.com>
2024-10-17 23:10:54 +0700

    fix create Consignment

--- src/main/java/com/group2/KoiFarmShop/controller/ConsignmentController.java
+++ src/main/java/com/group2/KoiFarmShop/controller/ConsignmentController.java
@@ -34,7 +34,8 @@ public class ConsignmentController {
     private EmailService emailService;
 
     @PostMapping("/createConsignment")
-    public ApiReponse<Integer> createConsignment(@RequestParam int accountId, @RequestParam MultipartFile koiImg,
+    public ApiReponse<Integer> createConsignment(@RequestParam int accountId, @RequestParam(required = false) MultipartFile koiImg,
+                                                 @RequestParam(required = false) String koiImgURL,
                                                 @RequestParam String origin,
                                                 @RequestParam boolean gender,
                                                 @RequestParam int age,
@@ -59,7 +60,7 @@ public class ConsignmentController {
     ) {
 
         ApiReponse<Integer> apiReponse = new ApiReponse<>();
-        apiReponse.setData(consignmentService.createConsignment(accountId, koiImg, origin, gender, age, size, personality, price, food, health, ph, temperature, water,
+        apiReponse.setData(consignmentService.createConsignment(accountId, koiImg,koiImgURL, origin, gender, age, size, personality, price, food, health, ph, temperature, water,
                 pureBred, categoryId, name, certImg, notes, phoneNumber, consignmentType, duration, serviceFee, online));
 
         return apiReponse;
